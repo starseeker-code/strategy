039650590e9e31604ab9481e3a5a9d398be5e3c9
Joaquin Hernandez Martinez <noethercode@gmail.com>
2023-07-09 12:51:43 +0200

    Terminado V2

--- strategy.py
+++ strategy.py
@@ -16,18 +16,22 @@ class IEstrategia(ABC):
 class Andar(IEstrategia):  # Cada estrategia contiene un algoritmo concreto
     def algoritmo(self):
         print(f"{self.nombre} no utilizara ningun metodo de transporte, y llegara con {self.tiempo - 60} minutos extra")
+        return 0  # Coste de andar
         
 class Bicicleta(IEstrategia):
     def algoritmo(self):
         print(f"{self.nombre} alquilara una bibicleta, y llegara con {self.tiempo - 45} minutos extra")
+        return 10  # Coste de alquilar una bicicleta
         
 class Coche(IEstrategia):
     def algoritmo(self):
         print(f"{self.nombre} alquilara un coche, y llegara con {self.tiempo - 25} minutos extra")
+        return 25
         
 class Taxi(IEstrategia):
     def algoritmo(self):
         print(f"{self.nombre} utilizara un taxi, y llegara con {self.tiempo - 12} minutos extra")
+        return 35
 
 # 3 - Se crea la interfaz de contexto
 
@@ -46,10 +50,11 @@ class Contexto:
     def estrategia(self, nueva_estrategia):
         self._estrategia = nueva_estrategia
         self._estrategia.nombre = self.nombre  # Se actualiza la informacion tambien
-        self._estrategia.tiempo = 12
+        self._estrategia.tiempo = 90  # !Override manual para que quede bonito
     
     def elegir_transporte(self):  # Se ejecuta la estrategia
         result = self._estrategia.algoritmo()
+        self.dinero -= result  # Añade el coste del transporte
         return result
 
     
@@ -60,6 +65,7 @@ if __name__ == '__main__':
     print(f"{nombre} tiene 100 € y necesita llegar al aeropuerto en {tiempo} minutos")
     contexto = Contexto(nombre, tiempo, estrategia=Bicicleta())  # En esta version no hay inyeccion de dependencias
     contexto.elegir_transporte()
-    print("En caso de elegir un taxi para volver...")
-    contexto.estrategia = Taxi()
+    print(f"A {contexto.nombre} le quedan {contexto.dinero}€")
+    print("En caso de elegir ir andando para volver...")
+    contexto.estrategia = Andar()
     contexto.elegir_transporte()
